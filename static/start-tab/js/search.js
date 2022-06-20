6551a6c73356ea8dc5e1e096a1ed95aa4564b724
Fajar Agung <fajarspace@192.168.1.7>
2022-06-20 11:14:49 +0700

    rebuilding site Mon Jun 20 11:14:49 WIB 2022

--- static/start-tab/js/search.js
+++
@@ -1,8 +0,0 @@
-const search = document.querySelector('#js-search');
-const searchInput = search.querySelector('input');
-search.addEventListener('submit', function(e){
-    e.preventDefault();
-    window.location.href = `https://google.com/search?sm=top_hty&fbm=1&ie=utf8&query=${searchInput.value}`;
-    searchInput.value = '';
-    return false;
-});
