1b3e85d9f4ff141f4015637eef6ff7d4dda77478
fajarspace <fajar.agngn@gmail.com>
2023-10-06 00:53:41 +0700

    rebuilding site Fri, Oct  6, 2023 12:53:41 AM

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import RootLayout from "./layout";
 import { content, metadata } from "@/theme.config";
 import Head from "next/head";
+import bukuCss from "../public/yaha.jpg";
+import Image from "next/image";
 
 const index = () => {
   return (
@@ -19,6 +21,16 @@ const index = () => {
         ></a>
       </p>
       <hr />
+      <Image src={bukuCss} width={300} height={300} alt="Buku css" />
+      <p>
+        Buku terbaru saya,{" "}
+        <a
+          href="https://api.whatsapp.com/send?phone=6289637524919&text=Hai%2C%20saya%20mau%20pesan%20buku%20Mudah%20membuat%20framework%20dari%20Nol"
+          target="_blank"
+        >
+          Pesan disini
+        </a>
+      </p>
     </RootLayout>
   );
 };
